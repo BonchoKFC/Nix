b79f6c6b89b40e0608cd57cffa1348772f9b9d33
BonchoKFC <138968045+BonchoKFC@users.noreply.github.com>
2024-03-13 13:08:05 -0400

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './navbar.css'
-import {RiMenu3Line, RiCloseLin} from 'react-icons/ri'
+import {RiMenu3Line, RiCloseLin, RiCloseLine} from 'react-icons/ri'
 import logo from '../../assets/logo.svg'
 
 
+const Menu = () => (
+<>
+<p><a href="#home">Home</a></p>
+<p><a href="#whatNix">Why Nix</a></p>
+<p><a href="#features">Features</a></p>
+<p><a href="#possibility">possibility</a></p>
+<p><a href="#blog">Blog</a></p>
+</>
+)
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="nix__navbar">
       <div className="nix__navbar__links">
@@ -12,17 +23,29 @@ const Navbar = () => {
           <img src={logo} alt ="logo"/>
         </div>
         <div className="nix__navbar-links_container"> 
-          <p><a href="#home">Home</a></p>
-          <p><a href="#whatNix">Why Nix</a></p>
-          <p><a href="#features">Features</a></p>
-          <p><a href="#possibility">possibility</a></p>
-          <p><a href="#blog">Blog</a></p>
+        <Menu/>
         </div>
       </div>
       <div className='nix__navbar-sign'>
         <p>Sign in</p>
         <button type="button">Sign up</button>
       </div>
+      <div className='nix__navbar-menu'> </div>{
+        toggleMenu
+        ? <RiCloseLine color="#fff" size ={27} onClick={() => setToggleMenu(false)}/>
+        : <RiMenu3Line color="#fff" size ={27} onClick={() => setToggleMenu(true)}/>
+      }
+      {toggleMenu &&(
+        <div className="nix__navbar-menu_container scale-up-center">
+          <div className='nix_navbar-menu_container-links'>
+            <Menu />
+            <div className='nix__navbar-sign-menu_container-links-sign'>
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
