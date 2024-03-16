7f09d2bcba02f5323804f19c39e0510190fc2665
BonchoKFC <138968045+BonchoKFC@users.noreply.github.com>
2024-03-16 00:19:03 -0400

    Possibility Section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import './possibility.css';
-const Possibility = () => {
-  return (
-    <div>Possibility</div>
-  )
-}
+import React from 'react';
+import possibilityImage from '../../assets/schedule2.webp';
+import './possibility.css'; // Ensure this file is correctly named and located
 
-export default Possibility
+const Possibility = () => (
+  <div className="nix__possibility section__padding" id="possibility">
+    <div className="nix__possibility-image">
+      <img src={possibilityImage} alt="schedule2" />
+    </div>
+    <div className="nix__possibility-content">
+      <h4>Embrace the Journey to Self-Improvement</h4>
+      <h1 className="gradient__text">Unlock Your Potential <br /> With Nix</h1>
+      <p>Embark on a path of self-discovery and improvement with our app. Let Nix guide you through setting realistic goals, improving daily habits, and unlocking the full potential of every day.</p>
+      <h4>Join Us Now for Early Access</h4>
+    </div>
+  </div>
+);
+
+export default Possibility;
