b5b6f28bf5aa54dada6448f444e62fd91e2608ce
BonchoKFC <138968045+BonchoKFC@users.noreply.github.com>
2024-03-16 00:12:44 -0400

    Added a WhatNix and Features Component

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import Feature from '../../components/feature/Feature';
 import './features.css';
-const Features = () => {
-  return (
-    <div>Features</div>
-  )
-}
 
-export default Features
+const featuresData = [
+  {
+    title: 'Maximize Your Day',
+    text: 'Unlock the potential of each day with tailored scheduling designed to empower your productivity and foster personal growth.',
+  },
+  {
+    title: 'Streamline Your Goals',
+    text: 'Visualize your ambitions and break them down into actionable steps. Our app helps you keep focus on achieving your long-term goals with daily reminders and progress tracking.',
+  },
+  {
+    title: 'Enhance Your Focus',
+    text: 'Eliminate distractions and enhance your focus with techniques and tools crafted to boost your concentration and efficiency.',
+  },
+  {
+    title: 'Transform Your Habits',
+    text: 'Identify, track, and transform your habits with our analytics tools. Make lasting changes that propel you towards your personal and professional aspirations.',
+  },
+];
+
+const Features = () => (
+  <div className="nix__features section__padding" id="features">
+    <div className="nix__features-heading">
+      <h1 className="gradient__text">Elevate Your Productivity & Well-being. Embrace the Art of Time Management.</h1>
+      <p>Join the movement towards self-improvement and effective time management. Get started today.</p>
+    </div>
+    <div className="nix__features-container">
+      {featuresData.map((item, index) => (
+        <Feature title={item.title} text={item.text} key={item.title + index} />
+      ))}
+    </div>
+  </div>
+);
+
+export default Features;
--- src/containers/whatNix/WhatNix.jsx
+++ src/containers/whatNix/WhatNix.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './whatNix.css';
-const WhatNix = () => {
-  return (
-    <div>WhatNix</div>
-  )
-}
+import Feature from '../../components/feature/Feature';
 
-export default WhatNix
+const WhatNix = () => (
+  <div className="nix__whatnix section__margin" id="whatNix">
+    <div className="nix__whatnix-feature">
+      <Feature title="Why Nix?" text="Dive into the heart of productivity and discover how Nix transforms your daily hustle into a balanced, goal-oriented routine. Embrace the art of managing time efficiently, turning aspirations into daily actions." />
+    </div>
+    <div className="nix__whatnix-heading">
+      <h1 className="gradient__text">Empower Your Days</h1>
+      <p>Uncover the Features</p>
+    </div>
+    <div className="nix__whatnix-container">
+      <Feature title="Personalized Planning" text="Tailor your time with personalized schedules that fit your lifestyle and ambitions. Nix provides the tools to plan your day effectively, ensuring every moment counts." />
+      <Feature title="Goal-Oriented Tasks" text="Set, track, and achieve your goals with Nix's intuitive task management system. Break down your long-term objectives into manageable daily tasks that propel you forward." />
+      <Feature title="Self-Improvement Insights" text="Gain insights into your productivity patterns. Nix's analytics help you understand how you spend your time and identify opportunities for growth and improvement." />
+    </div>
+  </div>
+);
+
+export default WhatNix;
