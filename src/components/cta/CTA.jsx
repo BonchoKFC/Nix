dd0a990d020cc1650f2e0bdc5e9e083c83f1c6a5
BonchoKFC <138968045+BonchoKFC@users.noreply.github.com>
2024-03-16 00:21:43 -0400

    CTA component

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
 import './cta.css';
 
-const CTA = () => {
-  return (
-    <div>CTA</div>
-  )
-}
+const CTA = () => (
+  <div className="nix__cta">
+    <div className="nix__cta-content">
+      <p>Unlock Your Potential Today</p>
+      <h3>Join our community & start your journey towards self-improvement.</h3>
+    </div>
+    <div className="nix__cta-btn">
+      <button type="button">Get Started</button>
+    </div>
+  </div>
+);
 
-export default CTA
+export default CTA;
